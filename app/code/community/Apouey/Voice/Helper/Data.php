e9bd796a1861dd484c127712d8c723db5aab900e
apouey <apouey@volcomeurope.com>
2014-11-17 00:09:32 +0100

    update

---
+++ app/code/community/Apouey/Voice/Helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Voice helper
+ *
+ * @author Alexandre Pouey <apouey@gmail.com>
+ * @package Apouey
+ * @module Voice
+ */
+class Apouey_Voice_Helper_Data extends Mage_Payment_Helper_Data
+{
+
+}
---
+++ app/code/community/Apouey/Voice/controllers/CommandController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Voice controller
+ *
+ * @author Alexandre Pouey <apouey@gmail.com>
+ * @package Apouey
+ * @module Voice
+ */
+class Apouey_Voice_CommandController extends Mage_Core_Controller_Front_Action
+{
+
+}
