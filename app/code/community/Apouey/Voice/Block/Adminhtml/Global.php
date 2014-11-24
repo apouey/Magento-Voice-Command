c2238e7299ccc9b66569dfd8c4fc83a4d736c9b0
apouey <apouey@volcomeurope.com>
2014-11-24 01:24:08 +0100

    no message

--- app/code/community/Apouey/Voice/Block/Adminhtml/Global.php
+++ app/code/community/Apouey/Voice/Block/Adminhtml/Global.php
@@ -2,12 +2,6 @@
 
 class Apouey_Voice_Block_Adminhtml_Global extends Mage_Adminhtml_Block_Template
 {
-	public function __construct()
-    {
-        parent::__construct();
-        $this->setTemplate('voice/global.phtml');
-    }
-
 	protected function getAdminLocale()
 	{
 		return Mage::app()->getLocale()->getLocaleCode();
